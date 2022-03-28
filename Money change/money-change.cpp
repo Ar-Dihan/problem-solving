3076e2536ec2d886691141777780507322711465
Ar-Dihan <arnahid18@gmail.com>
2022-03-28 23:07:22 +0600

    updated money machine

--- Money change/money-change.cpp
+++ Money change/money-change.cpp
@@ -4,28 +4,42 @@ using namespace std;
 int main()
 {
 
-    cout << "\n\nThat's the amount it can break into smallest possible number of bank notes.\n\n";
+    // cout << "\n\nThat's the amount it can break into smallest possible number of bank notes.\n\n";
 
-    int input, a100, b50, c20, d10, e5, f2, g1;
+    // int input, a100, b50, c20, d10, e5, f2, g1;
 
-    // user input:
-    cin >> input;
+    // // user input:
+    // cin >> input;
 
-    a100 = input / 100;
-    b50 = (input % 100) / 50;
-    c20 = ((input % 100) % 50) / 20;
-    d10 = (((input % 100) % 50) % 20) / 10;
-    e5 = ((((input % 100) % 50) % 20) % 10) / 5;
-    f2 = (((((input % 100) % 50) % 20) % 10) % 5) / 2;
-    g1 = ((((((input % 100) % 50) % 20) % 10) % 5) % 2) / 1;
+    // a100 = input / 100;
+    // b50 = (input % 100) / 50;
+    // c20 = ((input % 100) % 50) / 20;
+    // d10 = (((input % 100) % 50) % 20) / 10;
+    // e5 = ((((input % 100) % 50) % 20) % 10) / 5;
+    // f2 = (((((input % 100) % 50) % 20) % 10) % 5) / 2;
+    // g1 = ((((((input % 100) % 50) % 20) % 10) % 5) % 2) / 1;
 
-    cout << a100 << " 100 notes\n"
-         << b50 << " 50 notes\n"
-         << c20 << " 20 notes\n"
-         << d10 << " 10 notes\n"
-         << e5 << " 5 notes\n"
-         << f2 << " 2 notes\n"
-         << g1 << " 1 notes\n";
+    // cout << a100 << " 100 notes\n"
+    //      << b50 << " 50 notes\n"
+    //      << c20 << " 20 notes\n"
+    //      << d10 << " 10 notes\n"
+    //      << e5 << " 5 notes\n"
+    //      << f2 << " 2 notes\n"
+    //      << g1 << " 1 notes\n";
+
+    int a[9] = {1000, 500, 100, 50, 20, 10, 5, 2, 1};
+    int amount, i, temp;
+
+    cout << "\nPlease Enter the Amount of Cash =  ";
+    cin >> amount;
+
+    temp = amount;
+
+    for (i = 0; i < 9; i++)
+    {
+        cout << a[i] << " Notes is = " << temp / a[i] << endl;
+        temp = temp % a[i];
+    }
 
     return 0;
 }
